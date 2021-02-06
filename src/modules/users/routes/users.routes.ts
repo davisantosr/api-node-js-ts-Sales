1540829d0de013a1fe65702f5a2755f72a0125ca
davisantosr <davi-rocha2@hotmail.com>
2021-02-06 15:11:49 -0300

    add route to upload avatar

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import { celebrate, Joi, Segments } from 'celebrate'
+import multer from 'multer'
+import uploadConfig from '@config/upload'
 import UsersController from '../controllers/UsersController'
 import isAuthenticated from '../../../shared/http/middewares/isAuthenticated'
+import UserAvatarController from '../controllers/UserAvatarController'
 
 const usersRouter = Router()
 const usersController = new UsersController()
+const usersAvatarController = new UserAvatarController()
+
+const updload = multer(uploadConfig)
 
 usersRouter.get('/', isAuthenticated, usersController.index);
 
@@ -18,6 +24,13 @@ usersRouter.post(
     },
   }),
   usersController.create
+)
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  updload.single('avatar'),
+  usersAvatarController.update
 )
 
 export default usersRouter;
