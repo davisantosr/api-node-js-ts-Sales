70a3f4a40db0f7e1564104ffa85ee123b9aab33f
davisantosr <davi-rocha2@hotmail.com>
2021-02-13 22:41:14 -0300

    email to reset the password functionality

--- src/config/mail/HandlebarsMailTemplate.ts
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -1,17 +1,24 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
+import { privateEncrypt } from 'crypto';
 
 interface ITemplateVariable {
   [key: string]: string | number
 }
 
 interface IParseMailTemplate {
-  template: string;
+  file: string;
   variables: ITemplateVariable;
 }
 
 export default class HandlebarsMailTemplate {
-  public async parse({template, variables}: IParseMailTemplate): Promise<string> {
-    const parseTemplate = handlebars.compile(template)
+  public async parse({file, variables}: IParseMailTemplate): Promise<string> {
+
+    const templateFileContent = await fs.promises.readFile(file, {encoding: 'utf-8'});
+
+    console.log(templateFileContent)
+
+    const parseTemplate = handlebars.compile(templateFileContent)
 
     return parseTemplate(variables)
   }
