07fecba074b22abbe17aa272cd1907728ff25043
davisantosr <davi-rocha2@hotmail.com>
2021-02-13 21:04:25 -0300

    add handlebars| HandlebarsMailTemplate | initial change on EtherealMail

--- src/config/mail/EtherealMail.ts
+++ src/config/mail/EtherealMail.ts
@@ -1,14 +1,33 @@
 import nodemailer from 'nodemailer';
+import HandlebarsMailTemplate from './HandlebarsMailTemplate'
+
+interface IMailContact {
+  name: string;
+  email: string
+}
+
+interface ITemplateVariable {
+  [key: string]: string | number
+}
+
+interface IParseMailTemplate {
+  template: string;
+  variables: ITemplateVariable;
+}
 
 interface ISendMail {
-  to: string;
-  body: string;
-
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplate;
 }
+
 export default class EtherealMail {
-  static async sendMail({to, body}:ISendMail): Promise<void>{
+  static async sendMail({to, from, subject, templateData}:ISendMail): Promise<void>{
 
     const account = await nodemailer.createTestAccount();
+
+    const mailTemplate = new HandlebarsMailTemplate()
 
     const transporter = nodemailer.createTransport({
       host: account.smtp.host,
@@ -21,10 +40,16 @@ export default class EtherealMail {
     })
 
     const message = await transporter.sendMail({
-      from: 'email@email.com',
-      to,
-      subject: 'Password Recovery',
-      text: body
+      from: {
+        name: from?.name || 'Api Team',
+        address: from?.email || 'team@api.com'
+      },
+      to: {
+        name: to.name,
+        address: to.email
+      },
+      subject,
+      html: await mailTemplate.parse(templateData)
     })
 
     console.log('Message sent: %s', message.messageId)
